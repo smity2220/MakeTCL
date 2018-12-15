76d65283ba7fe4425a8198125d543d80c0eb9e40
smity2220 <smity2220@gmail.com>
2018-12-15 13:35:05 -0500

    Filling in some synthesis tool infrastructure. Not yet tested.

--- toolchains/synthesis/quartus/quartus.tcl
+++ toolchains/synthesis/quartus/quartus.tcl
@@ -1,12 +1,68 @@
 
 mTclLog 0 "MTCL QUARTUS currently supports the following versions... none"
 
-proc newProj{} {
-
+proc newProj {} {
+	# -overwrite
+	project_new $PROJECT_NAME -revision $PLATFORM
 }
 
-proc addToProj{} {
+proc addToProj {src_list} {
+	foreach filenm [dict keys $src_list] {
+		set library [dict get $src_list $filenm]
 
+		# Check for options added onto the end of the filename
+		set options {}
+		set fileopt [regexp -all -inline {\S+} $filenm]
+		if { [length $fileopt] > 1 } {
+			set filename [lindex $fileopt 0]
+			mTclLog 0 "MTCL - BUILD - DON'T USE FILE OPTIONS"
+			set options [lrange $fileopt 1 [llength $fileopt]]
+		} else {
+			set filename $filenm
+		}
+
+		# Check for options attached to the library
+		set libOpt [regexp -all -inline {\S+} $library]
+		if { [llength $libOpt] > 1 } {
+			# Pull out the library from the front of the list
+			set lib [lindex $libOpt 0]
+			# Save off the options
+			set opt [lrange $libOpt 1 [llength $libOpt]]
+			# Combine the two option sets
+			set options "$options $opt"
+		}
+
+		switch -glob [file extension $filename] {
+			".sv" {
+				set_global_assignment -name SYSTEMVERILOG_FILE $filename -library $lib
+			}
+			".v" {
+				set_global_assignment -name VERILOG_FILE $filename -library $lib
+			}
+			".vh*" - ".pkg" {
+				if {[lsearch $options "VHDL_2008"] >= 0} {
+					set_global_assignment -name VHDL_FILE $filename -hdl_version VHDL_2008 -library $lib	
+				} else {
+					set_global_assignment -name VHDL_FILE $filename -library $lib
+				}
+			}
+			".qip" {
+				set_global_assignment -name QIP_FILE $filename
+			}
+			".qsys" {
+				set_global_assignment -name QSYS_FILE $filename
+			}
+			".sdc" {
+				set_global_assignment -name SDC_FILE $filename
+			}
+			".tcl" {
+				mTclLog 0 "MTCL - BUILD - sourcing $filename"
+				# TODO: surround with catch
+				source $filename
+			}
+			default {}
+		}
+	}
 } 
 
 proc synth {} {
--- toolchains/synthesis/vivado/vivado.tcl
+++ toolchains/synthesis/vivado/vivado.tcl
@@ -10,10 +10,43 @@ mTclLog 0 "MTCL VIVADO currently supports the following versions... none"
 
 # } 
 
-# proc synth{platform} {
+proc synth {platform} {
+	open_project -quiet $BUILD_DIR/$PROJ_NAME/$PROJ_NAME.xpr
+	launch_runs synth_1
+	wait_on_run synth_1
+	close_project -quiet
+}
 
-# }
+proc impl {platform} {
+	open_project -quiet $BUILD_DIR/$PROJ_NAME/$PROJ_NAME.xpr
+	launch_runs impl_1 -to_step write_bitstream
+	wait_on_run impl_1
+	close_project -quiet
+}
 
-# proc impl{platform} {
+proc vivado_export_sdk {} {
+	open_project -quiet $BUILD_DIR/$PROJ_NAME/$PROJ_NAME.xpr
+	file mkdir @EXPORT_DIR
+	write_sysdef -force \
+	-hwdef "$BUILD_DIR/$PROJ_NAME/${PROJ_NAME}.runs/impl_1/$TARGET_PLATFORM.hwdef" \
+	-bitfile "$BUILD_DIR/$PROJ_NAME/${PROJ_NAME}.runs/impl_1/$TARGET_PLATFORM.bit" \
+	-meminfo "$BUILD_DIR/$PROJ_NAME/${PROJ_NAME}.runs/impl_1/$TARGET_PLATFORM.mmi" \
+	$EXPORT_DIR/$TARGET_PLATFORM.hwdef
+	close_project -quiet
+}
 
-# }
+proc vivado_add_elf {} {
+	open_project -quiet $BUILD_DIR/$PROJ_NAME/$PROJ_NAME.xpr
+	add_files -norecurse $ELF_FILENAME
+	set_property SCOPED_TO_CELLS {microblaze_0} [get_files $ELF_FILENAME]
+	set_property SCOPED_TO_REF mb [get_files $ELF_FILENAME]
+	close_project -quiet
+}
+
+proc vivado_bitgen {} {
+	open_project -quiet $BUILD_DIR/$PROJ_NAME/$PROJ_NAME.xpr
+	# Not sure if this is required
+	open_run impl_1
+	write_bitstream -force -no_partial_bitfile -bin_file "$BUILD_DIR/$PROJ_NAME/${PROJ_NAME}.runs/impl_1/$TARGET_PLATFORM"
+	close_project -quiet
+}
